5b2e8d8c393b1c1954688b13d1fab32d97cbb853
Anton Strokov <anton.strokoff@gmail.com>
2023-10-13 19:50:45 +0300

    some adds

---
+++ queue/main.py
@@ -0,0 +1,13 @@
+from queue import Queue
+
+def main():
+    q = Queue()
+    print(q)
+    # q.enqueue(11)
+    print(q)
+    aa = q.dequeue()
+    print(q)
+    print(aa.next)
+
+if __name__ == '__main__':
+    main()
---
+++ queue/queue.py
@@ -0,0 +1,43 @@
+from node import Node
+
+
+class Queue:
+
+    def __init__(self, *args):
+        self.__length = 0
+        self.first = None
+        self.last = None
+        if args:
+            for arg in args:
+                self.enqueue(arg)
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.__length == 0:
+            self.last = self.first = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.__length += 1
+
+    def dequeue(self):
+        if self.__length == 0:
+            raise IndexError('Dequeue from empty queue')
+        val = self.first
+        self.first = self.first.next
+        val.next = None
+        self.__length -= 1
+        return val
+
+    def __len__(self):
+        return self.__length
+
+    def __repr__(self):
+        return f'{[i for i in self]}'
+
+    def __iter__(self):
+        temp = self.first
+        while temp is not None:
+            val = temp
+            temp = temp.next
+            yield val
